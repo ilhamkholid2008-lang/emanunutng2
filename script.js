205fb28d37486fe62a9aa260746bc62b48d97127
ilhamnstn <ilhamkholid2008@gmail.com>
2026-04-22 16:07:03 +0800

    update perbaikan tampilan

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry, i) => {
     if (entry.isIntersecting) {
       setTimeout(() => {
-        entry.target.style.opacity = '1';
+        entry.target.style.opacity = '0';
         entry.target.style.transform = 'translateY(0)';
         entry.target.classList.add('visible');
       }, i * 80);
@@ -13,7 +13,7 @@ const observer = new IntersectionObserver((entries) => {
 }, { threshold: 0.05 });
 
 reveals.forEach(el => {
-  el.style.opacity = '0';
+  el.style.opacity = '1';
   el.style.transform = 'translateY(30px)';
   el.style.transition = 'opacity .6s ease, transform .6s ease';
   observer.observe(el);
@@ -21,7 +21,21 @@ reveals.forEach(el => {
 
 window.addEventListener('scroll', () => {
   const nav = document.querySelector('nav');
+
   nav.style.background = window.scrollY > 50
     ? 'rgb(0, 0, 0)'
-    : 'rgba(79, 143, 240, 0.96)';
+    : 'rgba(17, 99, 215, 0.96)';
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
 });
